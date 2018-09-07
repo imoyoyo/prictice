0990e71d8007e54604b86709e7b7a3a1eb8e596c
imoyoyo <dennis3439053@gmail.com>
2018-09-07 14:24:17 +0800

    first commit

---
+++ schedule/topic.js
@@ -0,0 +1,21 @@
+var topic = [
+    "シャルル",
+    "打ち上げ花火",
+    "なんでもないや",
+    "え？あぁ、そう。",
+    "フラジール",
+    "ロメオ"
+];
+
+var startDate = new Date();
+
+function setMonthAndDay(startMonth, startDay)
+{
+    startDate.setMonth(startMonth-1);
+    startDate.setDate(startDay);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+}
+
+setMonthAndDay(8,27);
---
+++ youtube/playList.js
@@ -0,0 +1,11 @@
+var playList = [
+    "OWFJYFk4NUU",
+    "SX_ViT4Ra7k",
+    "-Ma400hkLqU"
+];
+
+var playTime = [
+    [100,105],
+    [100,105],
+    [100,105]
+];
